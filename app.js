b3d9793421ec31f35bf290796c788bdae9f39c06
Justin Mello <justinmello32@hotmail.com>
2020-11-22 12:51:13 -0800

    Adding images and updating app.js

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.get("/", function(req,res){
 	res.render("index.html");
 });
 
+app.get("/desktop-suport", function(req,res){
+	res.render("desktop-suport.html");
+});
+
+app.get("/sys-admin", function(req,res){
+	res.render("sys-admin.html");
+});
+
+app.get("/network-admin", function(req,res){
+	res.render("network-admin.html");
+});
+
 
 //Starting  Server
 app.listen(process.env.PORT, process.env.IP, function(){
