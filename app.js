4f2ba6b659003013f1224753874a35b84cd80cde
Justin Mello <justinmello32@hotmail.com>
2020-11-22 13:07:23 -0800

    Adding in variables in routes, need to rename files.

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ app.use(express.static("public"));
 
 //Routing
 app.get("/", function(req,res){
-	res.render("index.html");
+	res.render("index.html", {"pageTitle"},"Getting Started in IT");
 });
 
 app.get("/desktop-suport", function(req,res){
-	res.render("desktop-support.html");
+	res.render("desktop-support.html", {"pageTitle"}, "Desktop Support");
 });
 
 app.get("/sys-admin", function(req,res){
-	res.render("sys-admin.html");
+	res.render("sys-admin.html", {"pageTitle"}, "System Administrator");
 });
 
 app.get("/network-admin", function(req,res){
-	res.render("network-admin.html");
+	res.render("network-admin.html", {"pageTitle"}, "Network Administrator");
 });
 
 
